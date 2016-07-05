c8af26436d2b289506cc4bc5cffc678f3001491a
mbusta305 <busta2356@gmail.com>
2016-07-05 13:59:57 -0400

    adding functions

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@
 
 
 //Add a new task
+var addTask = function() {
   //when button is pressed
   //create a task item with the text from #new-task:
     //input (checkbox)
@@ -11,8 +12,10 @@
     //button.edit
     //button.delete
     //Each elements, needs modified and appended
+}
 
 //Edit and existing Task
+var editTask = function () {
   //When the Edit button is pressed
   //if the class of the parent is .editMode
     //switch from .editMode
@@ -22,15 +25,25 @@
     //input value becomes the label's text
 
     //Toggle .editMode on the parent
+}
 
 //Delete a task
+var deleteTask = function () {
+
+}
   //when the delete button is pressed
     //remove the parent list item from the ul
 
+
 //Mark a task as completed-tasks
+var taskCompleted = function () {
   //when the checkbox is checked
     //append the list item to the #completed-tasks
+}
 
 //Mark a task as incomplete-tasks
+
+var taskIncomplete = function () {
   //when the checkbox is unchecked
     //append the list item to the #incompleted-tasks
+}
